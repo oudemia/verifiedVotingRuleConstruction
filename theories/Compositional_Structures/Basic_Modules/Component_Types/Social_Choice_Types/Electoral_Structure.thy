7649468c98fe94ac43c46bfba2cb45a82a5b344e
oudemia <oudemia@posteo.de>
2025-03-18 17:11:20 +0100

    simplified electoral struct locale by removing superfluous limit variable

--- theories/Compositional_Structures/Basic_Modules/Component_Types/Social_Choice_Types/Electoral_Structure.thy
+++ theories/Compositional_Structures/Basic_Modules/Component_Types/Social_Choice_Types/Electoral_Structure.thy
@@ -1,4 +1,11 @@
-theory Electoral_Structure 
+(*  File:       Electoral_Structure.thy
+    Copyright   2021  Karlsruhe Institute of Technology (KIT)
+*)
+\<^marker>\<open>creator "Henriette Färber, Karlsruhe Institute of Technology (KIT)"\<close>
+
+section \<open>Electoral Structure\<close>
+
+theory Electoral_Structure
   imports 
      Profile_Interpretations 
      Result_Interpretations
@@ -8,11 +15,12 @@ theory Electoral_Structure
 locale electoral_structure =
   ballot well_formed_ballot + result contenders for
   well_formed_ballot :: "'a set \<Rightarrow> 'b \<Rightarrow> bool" and
-  contenders :: "'a set \<Rightarrow> 'r set" + 
-  fixes limit_by_conts :: "'r set \<Rightarrow> 'b \<Rightarrow> 'b"
-  assumes
-    limit_inherit: "limit_by_conts R b = limit_ballot (affected_alts R) b"
+  contenders :: "'a set \<Rightarrow> 'r set"
 begin
+
+fun limit_by_conts :: "'r set \<Rightarrow> 'b \<Rightarrow> 'b" where
+"limit_by_conts R b = limit_ballot (affected_alts R) b"
+
 
 lemma well_formed_ballots_inherit:
 fixes
@@ -35,13 +43,12 @@ proof -
     using fp elem
     by (simp add: well_formed_profile_def)
   moreover have "limit_by_conts ?R (p' v) = limit_ballot (affected_alts ?R) (p' v)"
-    using limit_inherit 
-    by blast
+  by simp
   ultimately show "well_formed_ballot (affected_alts ?R) (p' v)"
-    by (simp add: lim limit_inherit limit_sound)
+    by (simp add: lim limit_sound)
 qed
 
-sublocale ballot well_formed_ballot empty_ballot prefers wins limit_ballot
+sublocale ballot
 by (simp add: ballot_axioms)
 
 sublocale result
@@ -51,43 +58,30 @@ end
 
 
 sublocale electoral_structure \<subseteq> ballot
-proof (unfold_locales)
-qed 
+by unfold_locales
 
 sublocale electoral_structure \<subseteq> result
-proof (unfold_locales)
-qed
+by unfold_locales
 
 
-fun limit_pref_to_alts :: "'a set \<Rightarrow> 'a Preference_Relation \<Rightarrow> 'a Preference_Relation" where
-"limit_pref_to_alts A b = (A \<times> A) \<inter> b"
+subsection \<open>Interpretations\<close>
+
 
 global_interpretation \<P>\<V>_\<S>\<C>\<F>:
   electoral_structure default_ballot_\<P>\<V> prefers_\<P>\<V> wins_\<P>\<V> limit_\<P>\<V>_ballot
-     limit_alts affected_alts_\<S>\<C>\<F> ballot_\<P>\<V> id limit_pref_to_alts
-proof (unfold_locales, clarsimp)
-  fix
-    R :: "'a set" and
-    b :: "'a Preference_Relation"
-  show "R \<times> R \<inter> b = {(a1, a2). (a1, a2) \<in> b \<and> a1 \<in> R \<and> a2 \<in> R}" by auto
-qed
+  limit_alts affected_alts_\<S>\<C>\<F> ballot_\<P>\<V> id
+by unfold_locales auto
 
-
-fun limit_app_to_alts :: "'a set \<Rightarrow> 'a Approval_Set \<Rightarrow> 'a Approval_Set" where
-"limit_app_to_alts A b = A \<inter> b"
-
-fun limit_app_to_comm :: "('a Committee) set \<Rightarrow> 'a Approval_Set \<Rightarrow> 'a Approval_Set" where
-"limit_app_to_comm C b = \<Union>C \<inter> b"
 
 global_interpretation \<A>\<V>_\<S>\<C>\<F>:
   electoral_structure default_ballot_\<A>\<V> prefers_\<A>\<V> wins_\<A>\<V> limit_\<A>\<V>_ballot
-     limit_alts affected_alts_\<S>\<C>\<F> ballot_\<A>\<V> id limit_app_to_alts
+     limit_alts affected_alts_\<S>\<C>\<F> ballot_\<A>\<V> id
 by unfold_locales auto
 
 
 sublocale committee_result \<subseteq> \<A>\<V>_committee:
   electoral_structure default_ballot_\<A>\<V> prefers_\<A>\<V> wins_\<A>\<V> limit_\<A>\<V>_ballot
-  limit_committees affected_alts_committee ballot_\<A>\<V> committees limit_app_to_comm
-by (simp add: \<A>\<V>_profile.ballot_axioms electoral_structure_axioms.intro electoral_structure_def result_axioms)
+  limit_committees affected_alts_committee ballot_\<A>\<V> committees
+by (simp add: \<A>\<V>_profile.ballot_axioms electoral_structure_def result_axioms)
 
 end
